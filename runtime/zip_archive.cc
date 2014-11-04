187cf4df66288052e7ff2a936167610d789e5798
nuclearmistake <nuclearmistake@gmail.com>
2014-11-04 08:13:16 -0500

    Revert "Fixes for openmaster zip handling with kitkat error handling"
    This reverts commit 7d8fee5c760c319b7649d7144ad8129388901c71.

--- runtime/zip_archive.cc
+++ runtime/zip_archive.cc
@@ -39,32 +39,32 @@ uint32_t ZipEntry::GetCrc32() {
 }
 
 
-bool ZipEntry::ExtractToFile(File& file) {
+bool ZipEntry::ExtractToFile(File& file, std::string* error_msg) {
   const int32_t error = ExtractEntryToFile(handle_, zip_entry_, file.Fd());
   if (error) {
-    PLOG(WARNING) << "Zip: failed to extract " << std::string(ErrorCodeString(error));
+    *error_msg = std::string(ErrorCodeString(error));
     return false;
   }
 
   return true;
 }
 
-MemMap* ZipEntry::ExtractToMemMap(const char* entry_filename) {
+MemMap* ZipEntry::ExtractToMemMap(const char* entry_filename, std::string* error_msg) {
   std::string name(entry_filename);
   name += " extracted in memory from ";
   name += entry_filename;
   UniquePtr<MemMap> map(MemMap::MapAnonymous(name.c_str(),
                                              NULL, GetUncompressedLength(),
-                                             PROT_READ | PROT_WRITE));
-  if (map.get() == NULL) {
-    LOG(ERROR) << "Zip: mmap for '" << entry_filename << "' failed";
-    return NULL;
+                                             PROT_READ | PROT_WRITE, error_msg));
+  if (map.get() == nullptr) {
+    DCHECK(!error_msg->empty());
+    return nullptr;
   }
 
   const int32_t error = ExtractToMemory(handle_, zip_entry_,
                                         map->Begin(), map->Size());
   if (error) {
-    PLOG(WARNING) << "ZipEntry: failed to extract to memmap " << std::string(ErrorCodeString(error));
+    *error_msg = std::string(ErrorCodeString(error));
     return nullptr;
   }
 
@@ -85,13 +85,13 @@ static void SetCloseOnExec(int fd) {
   }
 }
 
-ZipArchive* ZipArchive::Open(const char* filename) {
+ZipArchive* ZipArchive::Open(const char* filename, std::string* error_msg) {
   DCHECK(filename != nullptr);
 
   ZipArchiveHandle handle;
   const int32_t error = OpenArchive(filename, &handle);
   if (error) {
-    PLOG(WARNING) << "ZipArchive: failed to open " << std::string(ErrorCodeString(error));
+    *error_msg = std::string(ErrorCodeString(error));
     CloseArchive(handle);
     return nullptr;
   }
@@ -100,14 +100,14 @@ ZipArchive* ZipArchive::Open(const char* filename) {
   return new ZipArchive(handle);
 }
 
-ZipArchive* ZipArchive::OpenFromFd(int fd, const char* filename) {
+ZipArchive* ZipArchive::OpenFromFd(int fd, const char* filename, std::string* error_msg) {
   DCHECK(filename != nullptr);
   DCHECK_GT(fd, 0);
 
   ZipArchiveHandle handle;
   const int32_t error = OpenArchiveFd(fd, filename, &handle);
   if (error) {
-    PLOG(WARNING) << "Zip: failed to open from fd: " << std::string(ErrorCodeString(error));
+    *error_msg = std::string(ErrorCodeString(error));
     CloseArchive(handle);
     return nullptr;
   }
@@ -116,14 +116,14 @@ ZipArchive* ZipArchive::OpenFromFd(int fd, const char* filename) {
   return new ZipArchive(handle);
 }
 
-ZipEntry* ZipArchive::Find(const char* name) const {
+ZipEntry* ZipArchive::Find(const char* name, std::string* error_msg) const {
   DCHECK(name != nullptr);
 
   // Resist the urge to delete the space. <: is a bigraph sequence.
   UniquePtr< ::ZipEntry> zip_entry(new ::ZipEntry);
   const int32_t error = FindEntry(handle_, name, zip_entry.get());
   if (error) {
-    PLOG(WARNING) << "ZipEntry: failed to Find " << std::string(ErrorCodeString(error));
+    *error_msg = std::string(ErrorCodeString(error));
     return nullptr;
   }
 
