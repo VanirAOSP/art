3fbf2bfc7f61f8d57aa116fc18612a1cc09ee78f
Andreas Gampe <agampe@google.com>
2016-08-23 17:35:19 -0700

    ART: Remove vestiges of GCC
    We no longer support GCC.
    
    Change-Id: I869ac6e5dcb166747e45236a0d313c7672bbf2de
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- runtime/interpreter/interpreter_switch_impl.cc
+++ runtime/interpreter/interpreter_switch_impl.cc
@@ -632,10 +632,8 @@ JValue ExecuteSwitchImpl(Thread* self, const DexFile::CodeItem* code_item,
         break;
       }
 
-#if defined(__clang__)
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wfloat-equal"
-#endif
 
       case Instruction::CMPL_FLOAT: {
         PREAMBLE();
@@ -703,9 +701,7 @@ JValue ExecuteSwitchImpl(Thread* self, const DexFile::CodeItem* code_item,
         break;
       }
 
-#if defined(__clang__)
 #pragma clang diagnostic pop
-#endif
 
       case Instruction::CMP_LONG: {
         PREAMBLE();
