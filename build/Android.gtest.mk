c371206650b0e47887d3bbb286f1e3216cafa96e
Aart Bik <ajcbik@google.com>
2016-08-26 11:31:48 -0700

    A first implementation of a loop optimization framework.
    Rationale:
    We are planning to add more and more loop related optimizations
    and this framework provides the basis to do so. For starters,
    the framework optimizes dead induction, induction that can be
    replaced with a simpler closed-form, and eliminates dead loops
    completely (either pre-existing or as a result of induction
    removal).
    
    Speedup on e.g. Benchpress Loop is 73x (17.5us. -> 0.24us.)
    [with the potential for more exploiting outer loop too]
    
    Test: 618-checker-induction et al.
    
    Change-Id: If80a809acf943539bf6726b0030dcabd50c9babc
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- build/Android.gtest.mk
+++ build/Android.gtest.mk
@@ -286,6 +286,7 @@ COMPILER_GTEST_COMMON_SRC_FILES := \
   compiler/optimizing/induction_var_range_test.cc \
   compiler/optimizing/licm_test.cc \
   compiler/optimizing/live_interval_test.cc \
+  optimizing/loop_optimization_test.cc \
   compiler/optimizing/nodes_test.cc \
   compiler/optimizing/parallel_move_test.cc \
   compiler/optimizing/pretty_printer_test.cc \
