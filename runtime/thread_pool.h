d0b1ceae18e5f4c0cc60687865686b552b79f98f
Andreas Gampe <agampe@google.com>
2017-01-24 13:12:19 -0800

    ART: Add ThreadPool mode that creates peers
    Add a mode where worker threads in a thread pool will get a Java
    peer. Add a test.
    
    Bug: 31684920
    Test: m test-art-host-gtest-thread_pool_test
    Change-Id: I3654cc2be1294a79881b6ac9f84445a1e7f24b70
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- runtime/thread_pool.h
+++ runtime/thread_pool.h
@@ -97,7 +97,12 @@ class ThreadPool {
   // Remove all tasks in the queue.
   void RemoveAllTasks(Thread* self) REQUIRES(!task_queue_lock_);
 
-  ThreadPool(const char* name, size_t num_threads);
+  // Create a named thread pool with the given number of threads.
+  //
+  // If create_peers is true, all worker threads will have a Java peer object. Note that if the
+  // pool is asked to do work on the current thread (see Wait), a peer may not be available. Wait
+  // will conservatively abort if create_peers and do_work are true.
+  ThreadPool(const char* name, size_t num_threads, bool create_peers = false);
   virtual ~ThreadPool();
 
   // Wait for all tasks currently on queue to get completed.
@@ -146,6 +151,7 @@ class ThreadPool {
   uint64_t total_wait_time_;
   Barrier creation_barier_;
   size_t max_active_workers_ GUARDED_BY(task_queue_lock_);
+  bool create_peers_;
 
  private:
   friend class ThreadPoolWorker;
